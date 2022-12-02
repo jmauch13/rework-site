2318dc919992a145d30167ab4ea2fafcf604ee5d
Jennie Mauch <jenniemauch@me.com>
2022-12-02 12:40:47 -0500

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import './index.css';
+import Header from './components/Header';
+import Landing from './components/Landing';
+import Contact from './components/Contact';
+import About from './components/About';
+import Cart from './components/Cart';
+import Television from './components/Television';
+import Transcript from './components/Transcript';
+import Video from './components/Video';
+import Webcast from './components/Webcast';
+import Request from './components/Request';
+import Biographies from './components/Biographies';
+import Footer from './components/Footer';
+import Lfooter from './components/Lfooter';
+import Services from './components/Services';
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+    
+      return (
+        <>
+            
+            <Header />
+        
+        
+            <div className='pages'>
+                <Routes>
+                    <Route exact path='/' element={<Landing />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/television' element={<Television />} />
+                    <Route path='/transcript' element={<Transcript />} />
+                    <Route path='/video' element={<Video />} />
+                    <Route path='/cart' element={<Cart />} />
+                    <Route path='/webcast' element={<Webcast />} />
+                    <Route path='/contact' element={<Contact />} />
+                    <Route path='/request' element={<Request />} />
+                    <Route path='/biographies' element={<Biographies />} />
+                    <Route path='/footer' element={<Footer />} />
+                    <Route path='/lfooter' element={<Lfooter />} />
+                    <Route path='/services' element={<Services />} />
+                </Routes>
+            </div>
+        
+        </>
+    )    
+        
+    }
+    
 export default App;
